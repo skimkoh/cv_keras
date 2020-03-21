c9c6d417ba888ffcb4fcdb1f556000246405272e
kim <kohseukim@gmail.com>
2020-03-21 22:51:41 +0800

    frcnn model w changes

--- test_frcnn.py
+++ test_frcnn.py
@@ -11,6 +11,11 @@ from keras import backend as K
 from keras.layers import Input
 from keras.models import Model
 from keras_frcnn import roi_helpers
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+import pandas as pd
+import seaborn as sn 
+import matplotlib.pyplot as plt 
 
 sys.setrecursionlimit(40000)
 
@@ -147,101 +152,151 @@ bbox_threshold = 0.8
 
 visualise = True
 
+# test_numbers = 0
+miss_wrong_pred = 0
+# missed_count = {}
+
+pred = []
+label = []
+
 for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 	if not img_name.lower().endswith(('.bmp', '.jpeg', '.jpg', '.png', '.tif', '.tiff')):
 		continue
-	print(img_name)
-	st = time.time()
-	filepath = os.path.join(img_path,img_name)
+	print('img_name: {}'.format(img_name))
+	gold_label = img_name[:img_name.find('0')]
+	if gold_label == "mix":
+    		continue
+	else:
+		st = time.time()
+		filepath = os.path.join(img_path,img_name)
 
-	img = cv2.imread(filepath)
+		img = cv2.imread(filepath)
 
-	X, ratio = format_img(img, C)
+		X, ratio = format_img(img, C)
 
-	if K.image_dim_ordering() == 'tf':
-		X = np.transpose(X, (0, 2, 3, 1))
+		if K.image_dim_ordering() == 'tf':
+			X = np.transpose(X, (0, 2, 3, 1))
 
-	# get the feature maps and output from the RPN
-	[Y1, Y2, F] = model_rpn.predict(X)
-	
+		# get the feature maps and output from the RPN
+		[Y1, Y2, F] = model_rpn.predict(X)
+		
 
-	R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_dim_ordering(), overlap_thresh=0.7)
+		R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_dim_ordering(), overlap_thresh=0.7)
 
-	# convert from (x1,y1,x2,y2) to (x,y,w,h)
-	R[:, 2] -= R[:, 0]
-	R[:, 3] -= R[:, 1]
+		# convert from (x1,y1,x2,y2) to (x,y,w,h)
+		R[:, 2] -= R[:, 0]
+		R[:, 3] -= R[:, 1]
 
-	# apply the spatial pyramid pooling to the proposed regions
-	bboxes = {}
-	probs = {}
+		# apply the spatial pyramid pooling to the proposed regions
+		bboxes = {}
+		probs = {}
 
-	for jk in range(R.shape[0]//C.num_rois + 1):
-		ROIs = np.expand_dims(R[C.num_rois*jk:C.num_rois*(jk+1), :], axis=0)
-		if ROIs.shape[1] == 0:
-			break
+		for jk in range(R.shape[0]//C.num_rois + 1):
+			ROIs = np.expand_dims(R[C.num_rois*jk:C.num_rois*(jk+1), :], axis=0)
+			if ROIs.shape[1] == 0:
+				break
 
-		if jk == R.shape[0]//C.num_rois:
-			#pad R
-			curr_shape = ROIs.shape
-			target_shape = (curr_shape[0],C.num_rois,curr_shape[2])
-			ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
-			ROIs_padded[:, :curr_shape[1], :] = ROIs
-			ROIs_padded[0, curr_shape[1]:, :] = ROIs[0, 0, :]
-			ROIs = ROIs_padded
+			if jk == R.shape[0]//C.num_rois:
+				#pad R
+				curr_shape = ROIs.shape
+				target_shape = (curr_shape[0],C.num_rois,curr_shape[2])
+				ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
+				ROIs_padded[:, :curr_shape[1], :] = ROIs
+				ROIs_padded[0, curr_shape[1]:, :] = ROIs[0, 0, :]
+				ROIs = ROIs_padded
 
-		[P_cls, P_regr] = model_classifier_only.predict([F, ROIs])
+			[P_cls, P_regr] = model_classifier_only.predict([F, ROIs])
 
-		for ii in range(P_cls.shape[1]):
+			for ii in range(P_cls.shape[1]):
 
-			if np.max(P_cls[0, ii, :]) < bbox_threshold or np.argmax(P_cls[0, ii, :]) == (P_cls.shape[2] - 1):
-				continue
+				if np.max(P_cls[0, ii, :]) < bbox_threshold or np.argmax(P_cls[0, ii, :]) == (P_cls.shape[2] - 1):
+					continue
 
-			cls_name = class_mapping[np.argmax(P_cls[0, ii, :])]
+				cls_name = class_mapping[np.argmax(P_cls[0, ii, :])]
 
-			if cls_name not in bboxes:
-				bboxes[cls_name] = []
-				probs[cls_name] = []
+				if cls_name not in bboxes:
+					bboxes[cls_name] = []
+					probs[cls_name] = []
 
-			(x, y, w, h) = ROIs[0, ii, :]
+				(x, y, w, h) = ROIs[0, ii, :]
 
-			cls_num = np.argmax(P_cls[0, ii, :])
-			try:
-				(tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
-				tx /= C.classifier_regr_std[0]
-				ty /= C.classifier_regr_std[1]
-				tw /= C.classifier_regr_std[2]
-				th /= C.classifier_regr_std[3]
-				x, y, w, h = roi_helpers.apply_regr(x, y, w, h, tx, ty, tw, th)
-			except:
-				pass
-			bboxes[cls_name].append([C.rpn_stride*x, C.rpn_stride*y, C.rpn_stride*(x+w), C.rpn_stride*(y+h)])
-			probs[cls_name].append(np.max(P_cls[0, ii, :]))
+				cls_num = np.argmax(P_cls[0, ii, :])
+				try:
+					(tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
+					tx /= C.classifier_regr_std[0]
+					ty /= C.classifier_regr_std[1]
+					tw /= C.classifier_regr_std[2]
+					th /= C.classifier_regr_std[3]
+					x, y, w, h = roi_helpers.apply_regr(x, y, w, h, tx, ty, tw, th)
+				except:
+					pass
+				bboxes[cls_name].append([C.rpn_stride*x, C.rpn_stride*y, C.rpn_stride*(x+w), C.rpn_stride*(y+h)])
+				probs[cls_name].append(np.max(P_cls[0, ii, :]))
 
-	all_dets = []
+		all_dets = []
+		
+		# count = 0
 
-	for key in bboxes:
-		bbox = np.array(bboxes[key])
+		for key in bboxes:
+				
+			bbox = np.array(bboxes[key])
 
-		new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.5)
-		for jk in range(new_boxes.shape[0]):
-			(x1, y1, x2, y2) = new_boxes[jk,:]
+			new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.5)
+			for jk in range(new_boxes.shape[0]):
+					# if count == 0:
+						(x1, y1, x2, y2) = new_boxes[jk,:]
+						(real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
 
-			(real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
+						cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])),2)
 
-			cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])),2)
+						textLabel = '{}: {}'.format(key,int(100*new_probs[jk]))
+						all_dets.append((key,100*new_probs[jk]))
 
-			textLabel = '{}: {}'.format(key,int(100*new_probs[jk]))
-			all_dets.append((key,100*new_probs[jk]))
+						(retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
+						textOrg = (real_x1, real_y1-0)
 
-			(retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
-			textOrg = (real_x1, real_y1-0)
-
-			cv2.rectangle(img, (textOrg[0] - 5, textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (0, 0, 0), 2)
-			cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
-			cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+						cv2.rectangle(img, (textOrg[0] - 5, textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (0, 0, 0), 2)
+						cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
+						cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+						# count += 1
+				
 
 	print('Elapsed time = {}'.format(time.time() - st))
 	print(all_dets)
-	cv2.imshow('img', img)
-	cv2.waitKey(0)
-	# cv2.imwrite('./results_imgs/{}.png'.format(idx),img)
+	# cv2.imshow('img', img)
+	# cv2.waitKey(0)
+	# print('idx: {}'.format())
+	# cv2.imwrite('/Users/kohseukim/cv_project/results_test2/{}.png'.format(idx),img)
+	cv2.imwrite('/content/results/{}.png'.format(idx), img)
+	if len(all_dets):
+		pred_class  = (all_dets[0])[0]   # predicted class
+		pred.append(pred_class)
+		label.append(gold_label)   # gold truth class
+	else:
+			# pred.append('bg')
+			# label.append(gold_label)   # gold truth class
+			miss_wrong_pred += 1
+	
+	
+class_label = ['apple','banana','bread','bun','doughnut','egg','fired_dough_twist',
+               'grape','lemon','litchi','mango','mooncake','orange','peach','pear',
+               'plum', 'qiwi','sachima','tomato']
+
+# class_label = ['apple', 'banana', 'bread', 'doughnut', 'fired_dough_twist', 'litchi', 'peach', 'tomato', 'mango']
+print('no. of missed label')
+print(miss_wrong_pred)
+print('classification report')
+print(classification_report(label, pred, target_names=class_label))
+print('confusion matrix output')
+array = confusion_matrix(label, pred)
+df_cm = pd.DataFrame(array, index = [i for i in class_label], 
+        columns = [i for i in class_label]) 				
+# plt.figure(figsize = (40,40)) 
+# sn.set(font_scale=200) #for label size 
+sn.heatmap(df_cm, annot=True, annot_kws={"size": 12})   
+plt.savefig("/content/matrix.png")
+
+# sn.set(font_scale=1.4) # for label size
+# sn.heatmap(df_cm, annot=True, annot_kws={"size": 16}) # font size
+# plt.show()
+# plt.savefig("/content/matrix.png")
